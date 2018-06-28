9fa2f451d1c63cd5b58f31efa01dcb401da1f59d
Lee YH <mrzero0118@naver.com>
2018-06-28 15:22:56 +0900

    1806281522

--- jixxSpring4/src/main/java/com/kitri/project/repository/Service.java
+++ jixxSpring4/src/main/java/com/kitri/project/repository/Service.java
@@ -3,13 +3,19 @@ package com.kitri.project.repository;
 import vo.Repository;
 
 public interface Service {
-	Repository selectUrl(String url);
+	Repository selectRepByUrl(String url);
 	
 	void addRep(Repository r);
+	
+	Repository selectRepByName(Repository r);
 	
 	void editRep(Repository r);
 	
 	void delRep(int rep_id);
+
+	
+
+	
 
 	
 
