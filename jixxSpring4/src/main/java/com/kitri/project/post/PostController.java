e938e869d631812525936cc62663c173a536e452
KITRI-PC\KITRI <hyunwoo4973>
2018-06-28 15:22:29 +0900

    post insert 완성

--- jixxSpring4/src/main/java/com/kitri/project/post/PostController.java
+++ jixxSpring4/src/main/java/com/kitri/project/post/PostController.java
@@ -1,23 +1,24 @@
 package com.kitri.project.post;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import vo.Post;
 
 @Controller
 public class PostController {
+	@Resource(name="postService")
 	private Service service;
 	@RequestMapping(value = "/post/write.do")
 	public String write(HttpServletRequest req, Post post) {
 		HttpSession session = req.getSession(false);
-		post.setUser_id(Integer.parseInt((String) session.getAttribute("id")));
-		service.write(post);
+		post.setUser_id(Integer.parseInt(session.getAttribute("id").toString()));
 		System.out.println(post);
+		service.write(post);
 		return "template/main";
 	}
 }
